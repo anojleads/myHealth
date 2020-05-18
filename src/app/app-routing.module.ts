c7bbfca5cd48d380a475594bf88fc31420560871
unknown <anojleads@gmail.com>
2020-05-18 17:10:11 +0200

    info page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {CartComponent} from "./components/cart/cart.component";
 import {CheckoutComponent} from "./components/checkout/checkout.component";
 import {ProductComponent} from "./components/product/product.component";
 import {ThankyouComponent} from "./components/thankyou/thankyou.component";
-
+import {InfoComponent} from "./components/info/info.component"
 
 const routes: Routes = [
   {
@@ -30,6 +30,9 @@ const routes: Routes = [
   },
   {
     path: 'thankyou', component: ThankyouComponent
+  },
+  {
+  path:'info', component:InfoComponent
   }
 ];
 
